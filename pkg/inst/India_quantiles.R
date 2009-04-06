8840cf97c18576ce6334a0b9e678910293062354
kneib <kneib@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-04-06 09:54:27 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/mboost@155 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/India_quantiles.R
+++ pkg/inst/India_quantiles.R
@@ -1,6 +1,8 @@
 
-# Note: Requires Data File IAKR51FL.DTA
+# Note: Requires Data File IAKR51FL.DTA 
+# (Children`s recode for the 2005/2006 India DHS in Stata format)
 # Freely available from www.measuredhs.com for research purposes
+# (-> Data -> Available Datasets -> India)
 
 # Preprocessing of the raw data
 source("india_preproc.R", echo=TRUE)
