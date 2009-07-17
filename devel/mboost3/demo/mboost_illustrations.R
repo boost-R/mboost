7df9692f68273e6e72f59350ba0bd1380c079a7d
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-07-17 11:44:37 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/mboost@210 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- devel/mboost3/demo/mboost_illustrations.R
+++ devel/mboost3/demo/mboost_illustrations.R
@@ -1,16 +1,12 @@
-
-library("splines")
+###################################################
+### chunk number 1: pkg-attach
+###################################################
+source("setup.R")
 library("Matrix")
 library("mboost")
 
 f <- list.files(path = "../R", pattern = "R$", full = TRUE)
 sapply(f, source)
-
-
-###################################################
-### chunk number 1: pkg-attach
-###################################################
-source("setup.R")
 
 
 ###################################################
@@ -23,17 +19,33 @@ coef(bf_lm)
 ###################################################
 ### chunk number 3: bodyfat-glmboost-fit
 ###################################################
-bf_glm <- Glmboost(DEXfat ~ ., data = bodyfat, center = TRUE)
+bf_glm <- glmboost(DEXfat ~ ., data = bodyfat, control = boost_control(center = TRUE))
+bf_glm3 <- Glmboost(DEXfat ~ ., data = bodyfat, control = boost_control(center = TRUE))
 
 
 ###################################################
 ### chunk number 4: bodyfat-glmboost-coef
 ###################################################
-coef(bf_glm)
+max(abs(coef(bf_glm) - coef(bf_glm3)))
+
+max(abs(predict(bf_glm, newdata = bodyfat[1:10]) - 
+        predict(bf_glm3, newdata = bodyfat[1:10])))
+
+###################################################
+### chunk number 9: bodyfat-pkg-attach
+###################################################
+source("setup.R")
 
 
-bf_gam <- Gamboost(DEXfat ~ ., data = bodyfat)
-coef(bf_gam)
+###################################################
+### chunk number 10: bodyfat-gamboost-fit
+###################################################
+bf_gam <- gamboost(DEXfat ~ ., data = bodyfat)
+bf_gam3 <- Gamboost(DEXfat ~ ., data = bodyfat)
+sapply(1:length(coef(bf_gam)), function(i) max(abs(coef(bf_gam)[[i]] - coef(bf_gam3)[[i]])))
+
+max(abs(predict(bf_gam, newdata = bodyfat[1:10]) -
+        predict(bf_gam3, newdata = bodyfat[1:10])))
 
 
 ###################################################
@@ -55,8 +67,12 @@ bsfm
 ### chunk number 15: bodyfat-fpboost-fit
 ###################################################
 ctrl <- boost_control(mstop = 5000)
-bf_bs <- Glmboost(bsfm, data = bodyfat, control = ctrl)
-coef(bf_bs)
+bf_bs <- glmboost(bsfm, data = bodyfat, control = ctrl)
+bf_bs3 <- Glmboost(bsfm, data = bodyfat, control = ctrl)
+max(abs(coef(bf_bs) - coef(bf_bs3)))
+
+max(abs(predict(bf_bs, newdata = bodyfat[1:10]) -
+        predict(bf_bs3, newdata = bodyfat[1:10])))
 
 
 ###################################################
@@ -88,16 +104,27 @@ AIC(wpbc_step)
 ### chunk number 20: wpbc-glmboost-fit
 ###################################################
 ### fit logistic regression model via gradient boosting
-ctrl <- boost_control(mstop = 500)
-wpbc_glm <- Glmboost(status ~ ., data = wpbc2, family = Binomial(), center = TRUE,
+ctrl <- boost_control(mstop = 500, center = TRUE)
+wpbc_glm <- glmboost(status ~ ., data = wpbc2, family = Binomial(),
                      control = ctrl)
-coef(wpbc_glm)
+wpbc_glm3 <- Glmboost(status ~ ., data = wpbc2, family = Binomial(),
+                     control = ctrl)
+max(abs(coef(wpbc_glm) - coef(wpbc_glm3)))
+
+max(abs(predict(wpbc_glm, newdata = wpbc2[1:10,]) -
+        predict(wpbc_glm3, newdata = wpbc2[1:10,])))
+
 
 ###################################################
 ### chunk number 23: wpbc-gamboost-fit
 ###################################################
-wpbc_gam <- Gamboost(status ~ ., data = wpbc2, family = Binomial())
-coef(wpbc_gam)
+wpbc_gam <- gamboost(status ~ ., data = wpbc2, family = Binomial())
+wpbc_gam3 <- Gamboost(status ~ ., data = wpbc2, family = Binomial())
+sapply(1:length(coef(wpbc_gam)), function(i) max(abs(coef(wpbc_gam)[[i]] - coef(wpbc_gam3)[[i]])))
+
+max(abs(predict(wpbc_gam, newdata = wpbc2[1:10,]) -
+        predict(wpbc_gam3, newdata = wpbc2[1:10,])))
+
 
 ###################################################
 ### chunk number 25: pkg-attach
@@ -117,8 +144,21 @@ wpbc3 <- wpbc[,names(wpbc) != "status"]
 ###################################################
 ### chunk number 27: wpbc-glmboost-censored-fit
 ###################################################
-ctrl <- boost_control(mstop = 500)
-wpbc_surv <- Glmboost(log(time) ~ ., data = wpbc3,
-                  control = ctrl, weights = iw, center = TRUE)
-coef(wpbc_surv)
-names(coef(wpbc_surv)[abs(coef(wpbc_surv)) > 0])   
+ctrl <- boost_control(mstop = 500, center = TRUE)
+wpbc_surv <- glmboost(log(time) ~ ., data = wpbc3,
+                  control = ctrl, weights = iw)
+wpbc_surv3 <- Glmboost(log(time) ~ ., data = wpbc3,
+                  control = ctrl, weights = iw)
+max(abs(coef(wpbc_surv) - coef(wpbc_surv3)))
+
+max(abs(predict(wpbc_surv, newdata = wpbc3[1:10,]) -
+        predict(wpbc_surv3, newdata = wpbc3[1:10,])))
+
+
+###################################################
+### chunk number 28: wpbc-glmboost-coef
+###################################################
+names(coef(wpbc_surv)[abs(coef(wpbc_surv)) > 0])
+
+
+
