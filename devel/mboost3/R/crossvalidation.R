b7dd641bdcad32719dec5e870d218d2679a57c6b
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-07-30 11:12:50 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/mboost@238 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- devel/mboost3/R/crossvalidation.R
+++ devel/mboost3/R/crossvalidation.R
@@ -4,7 +4,7 @@
 ## for boosting algorithms
 ##
 
-Cvrisk <- function(object, folds = cv(model.weights(object)), grid = 1:mstop(object), 
+Cvrisk <- function(object, folds = cv(model.weights(object)), grid = 1:mstop(object),
                    parallel = require("multicore"), fun = NULL, ...) {
 
     weights <- model.weights(object)
@@ -28,7 +28,7 @@ Cvrisk <- function(object, folds = cv(model.weights(object)), grid = 1:mstop(obj
         dummyfct <- function(weights) {
             mod <- fitfct(weights = weights)
             mod[max(grid)]
-            mod$risk()[grid]
+            mod$risk()
         }
     } else {
         dummyfct <- function(weights) {
