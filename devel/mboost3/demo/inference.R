25fa9bbe18784dd25d1a32c42d2264e4f1c465e4
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-08 13:29:17 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/mboost@266 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- devel/mboost3/demo/inference.R
+++ devel/mboost3/demo/inference.R
@@ -1,20 +1,19 @@
 
 library("mboost3")
 
-
 data("bodyfat", package = "mboost")
 
 object <- glmboost(DEXfat ~ ., data = bodyfat, center = TRUE)
-a <- basesel(object, folds = cv(model.weights(object), type = "subsampling", B = 100))
+a <- mboost3:::basesel(object, folds = cv(model.weights(object), type = "subsampling", B = 100))
 
 object <- gamboost(DEXfat ~ ., data = bodyfat)
-a <- basesel(object)
+a <- mboost3:::basesel(object)
 matplot(t(a), type = "l")
 
 apply(a, 1, max)
 
 nd <- data.frame(hipcirc = 90:120)
-b <- fitsel(object, newdata = nd, which = "hipcirc")
+b <- mboost3:::fitsel(object, newdata = nd, which = "hipcirc")
 
 apply(b, 1, max)
 
@@ -22,9 +21,8 @@ n <- 500
 x <- seq(from = 0, to = 6 * pi, length = n)
 y <- 2 * sin(x) + rnorm(n)
 object <- gamboost(y ~ x)
-d <- fitsel(object, which = 1)
+d <- mboost3:::fitsel(object, which = 1)
 a <- apply(d, 1, max)
 plot(x, fitted(object), col = (a > 0.99) + 1)
 lines(x, 2 * sin(x))
 abline(h = 0)
-
