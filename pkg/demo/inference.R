0a721e16c6448f3433a707201777990a2eaf985f
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-01-07 13:46:39 +0000

    + stability selection
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/mboost@356 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/demo/inference.R
+++ pkg/demo/inference.R
@@ -4,25 +4,11 @@ library("mboost")
 data("bodyfat", package = "mboost")
 
 object <- glmboost(DEXfat ~ ., data = bodyfat, center = TRUE)
-a <- mboost:::basesel(object, folds = cv(model.weights(object), type = "subsampling", B = 100))
+a <- stabsel(object, cutoff = 0.9)
+a
+plot(a)
 
 object <- gamboost(DEXfat ~ ., data = bodyfat)
-a <- mboost:::basesel(object)
-matplot(t(a), type = "l")
-
-apply(a, 1, max)
-
-nd <- data.frame(hipcirc = 90:120)
-b <- mboost:::fitsel(object, newdata = nd, which = "hipcirc")
-
-apply(b, 1, max)
-
-n <- 500
-x <- seq(from = 0, to = 6 * pi, length = n)
-y <- 2 * sin(x) + rnorm(n)
-object <- gamboost(y ~ x)
-d <- mboost:::fitsel(object, which = 1)
-a <- apply(d, 1, max)
-plot(x, fitted(object), col = (a > 0.99) + 1)
-lines(x, 2 * sin(x))
-abline(h = 0)
+a <- stabsel(object, cutoff = 0.9)
+a
+plot(a)
