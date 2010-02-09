b048d404ad1995167b9906807f51b7d76f8f6893
fabian <fabian@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-09 17:14:43 +0000

    rm ugliness (thx benni)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/mboost@418 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/regtest-family.R
+++ pkg/tests/regtest-family.R
@@ -140,7 +140,6 @@ model2$scale
 data("wpbc", package = "mboost")
 wpbc[,colnames(wpbc) != "status"] <- scale(wpbc[,colnames(wpbc) != "status"])
 wpbc <- wpbc[complete.cases(wpbc), colnames(wpbc) != "time"]
-try(rm(ind1, ind0, n1, n0), sil=T) #avoid warning from AUC's check_y()
 mAUC <- gamboost(status ~ ., data = wpbc, family = AUC())
 1 - mAUC$risk()
 
