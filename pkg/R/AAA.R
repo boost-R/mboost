8070de57f6c53b2be2806fb8aa73209144783213
hofner <hofner@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-03 13:07:19 +0000

    removed startup message (commented out)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/mboost@523 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/AAA.R
+++ pkg/R/AAA.R
@@ -1,27 +1,27 @@
 
-.onAttach <- function(libname, pkgname) {
-
-    sup <- file.path(libname, pkgname, "startup.txt")
-    if (file.exists(sup) || !interactive()) return(TRUE)
-    if (!suppressWarnings(file.create(sup))) return(TRUE)
-    file.remove(sup)
-    version <- packageDescription(pkg = pkgname)$Version
-    txt <- c("\n",
-             paste("	Welcome to ", sQuote("mboost"),
-                   " version ", version, "!", sep = ""),
-             "\n",
-             "The user-interface changed in some places.",
-             paste("Most important, subsetting an", sQuote("mboost"),
-                   "object modifies this object now."),
-             "Please read the NEWS file, consult the documentation and have fun!",
-             "\n",
-             "Would you like to see this message on startup again?")
-    writeLines(txt)
-    choice <- menu(c("Please, no!", "Yes, please!"))
-    if (choice == 2) return(TRUE)
-    file.create(sup)
-    return(TRUE)
-}
+#.onAttach <- function(libname, pkgname) {
+#
+#    sup <- file.path(libname, pkgname, "startup.txt")
+#    if (file.exists(sup) || !interactive()) return(TRUE)
+#    if (!suppressWarnings(file.create(sup))) return(TRUE)
+#    file.remove(sup)
+#    version <- packageDescription(pkg = pkgname)$Version
+#    txt <- c("\n",
+#             paste("	Welcome to ", sQuote("mboost"),
+#                   " version ", version, "!", sep = ""),
+#             "\n",
+#             "The user-interface changed in some places.",
+#             paste("Most important, subsetting an", sQuote("mboost"),
+#                   "object modifies this object now."),
+#             "Please read the NEWS file, consult the documentation and have fun!",
+#             "\n",
+#             "Would you like to see this message on startup again?")
+#    writeLines(txt)
+#    choice <- menu(c("Please, no!", "Yes, please!"))
+#    if (choice == 2) return(TRUE)
+#    file.create(sup)
+#    return(TRUE)
+#}
 
 .onLoad <- function(libname, pkgname) {
     if (options("expressions")[[1]] <= 5000)
