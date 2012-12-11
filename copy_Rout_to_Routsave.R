60f05d5b2c518b9792749fbc9900ccee715dc27d
hofner <hofner@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-12-11 16:24:26 +0000

    fixed vignette outputs
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/mboost@685 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- copy_Rout_to_Routsave.R
+++ copy_Rout_to_Routsave.R
@@ -1,14 +1,18 @@
-######
+################################################################################
+#
 # USE THIS SCRIPT TO REPLACE ALL OCCURENCES OF *.Rout.save WITH NEW *.Rout FILES
 #
 # Author: Benjamin Hofner, 2012
-
-
+#
+# USAGE:
+#   Use either
+#     R CMD BATCH "--args which='mboostDevel'" copy_Rout_to_Routsave.R
+#   or
+#     R CMD BATCH "--args which='mboostPatch'" copy_Rout_to_Routsave.R
+#
 ################################################################################
-# Extract command line arguments specified via:
-#   R CMD BATCH "--args which='mboostDevel'" copy_Rout_to_Routsave.R
-# or
-#   R CMD BATCH "--args which='mboostPatch'" copy_Rout_to_Routsave.R
+
+## Get command line arguments
 args <- commandArgs(TRUE)
 if (length(args) > 1)
     stop("specify (at maximum) one argument (i.e., which)")
@@ -27,8 +31,11 @@ if (which == "mboostDevel") {
         stop("which is not correctly specified")
     }
 }
-################################################################################
 
+################################################################################
+## Copy output of test files
+
+## Get relevant file names
 ROUT <- list.files(path = check_path, pattern = ".Rout$", recursive = TRUE)
 ROUT2 <- paste(check_path, ROUT, sep ="")
 
@@ -56,3 +63,42 @@ cat("#########################################################################",
            "#   svn revert --recursive pkg/mboostPatch/tests"),
     "#########################################################################",
     sep = "\n")
+
+################################################################################
+## Copy output of vignettes
+
+vpath <- paste(path, "vignettes", sep ="/")
+
+## get vignette output as created by R CMD check:
+vROUT <- list.files(path = check_path, pattern = ".Rnw.log$")
+if (length(vROUT) > 0) {
+    vROUT2 <- paste(check_path, vROUT, sep ="")
+
+    vROUT.SAVE <- list.files(path = vpath, pattern = ".Rout.save",
+                             recursive = TRUE)
+    vROUT.SAVE <- paste(vpath, vROUT.SAVE, sep = "/")
+
+    ## sort
+    filenames <- gsub("(.*)\\.Rnw\\.log", "\\1", vROUT)
+    idx <- sapply(filenames, function(fn)
+                  res <- grep(paste(fn, "\\.Rout\\.save", sep=""), vROUT.SAVE))
+
+    vROUT.SAVE <- vROUT.SAVE[idx]
+
+    cbind(vROUT2, vROUT.SAVE)
+
+    cat("\n\nCopy *.Rnw.log to *.Rout.save:\n---------------------------\n")
+
+    for (i in 1:length(vROUT))
+        print(file.copy(vROUT2[i], vROUT.SAVE[i], overwrite = TRUE))
+
+    cat("#########################################################################",
+    "# To revert changes simply use:",
+    ifelse(which == "mboostDevel",
+           "#   svn revert --recursive pkg/mboostDevel/vignettes",
+           "#   svn revert --recursive pkg/mboostPatch/vignettes"),
+    "#########################################################################",
+    sep = "\n")
+} else {
+    cat("\n\nNOTE: No changes in output of vignettes.\n\n")
+}
