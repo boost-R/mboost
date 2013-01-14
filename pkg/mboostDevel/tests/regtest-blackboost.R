342efa318652a76ec5b9ae900d69c7501c905746
hofner <hofner@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-14 15:29:42 +0000

    release mboost 2.2-1
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/mboost@694 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mboostDevel/tests/regtest-blackboost.R
+++ pkg/mboostDevel/tests/regtest-blackboost.R
@@ -53,11 +53,11 @@ fit2 <- blackboost(Surv(futime,fustat) ~ age + resid.ds + rx + ecog.ps,
     data = ovarian, family = CoxPH(), control = boost_control(mstop = 1000))
 
 A2 <- survFit(fit2)
-A2
+print(A2)
 
 newdata <- ovarian[c(1,3,12),]
 A2 <- survFit(fit2, newdata = newdata)
-A2
+print(A2)
 
 ### predictions:
 set.seed(1907)
