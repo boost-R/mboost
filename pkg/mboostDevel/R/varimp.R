21e324847bb809de8cee0d65f66925c63ce0eb47
tkuehn13 <tobi.kuehn@gmx.de>
2016-02-28 09:22:46 +0100

    combine steps for blearner/variable plot

--- pkg/mboostDevel/R/varimp.R
+++ pkg/mboostDevel/R/varimp.R
@@ -1,14 +1,16 @@
 varimp <- function(object, ...)
   UseMethod("varimp")
 
-varimp.mboost <- function(object) {
-  
-  ### which baselearners were selected in boosting steps
-  # differentiation between gamboost- and glmboost-object
-  blearner_names <- if( !(inherits(object, "glmboost")) ) 
+
+varimp.mboost <- function(object) {  
+
+  ### which baselearners/variables were selected in boosting steps
+  learner_names <- if( !(inherits(object, "glmboost")) ) 
     names(object$baselearner) else variable.names(object)  
-  blearner_selected <- object$xselect()
-   
+  learner_selected <- object$xselect()
+  
+  
+  ### --------------------------------------------------
   ### compute risks for each step
   # initial risk for the intercept model
   y <- object$response
@@ -19,25 +21,33 @@ varimp.mboost <- function(object) {
   # risk reduction per step
   riskdiff <- c(risk0, riskstep[-length(riskstep)]) - riskstep  
   
-  ### compute empirical inbag risk (according to output in cvrisk)
+  ### compute empirical in-bag risk (according to output in cvrisk)
   riskdiff <- riskdiff / length(object$response)
   
-  ### explained Risk attributed to baselearners
-  explained <- sapply(seq_along(blearner_names), FUN = function(i) {
-    sum( riskdiff[which(blearner_selected == i)] ) 
-  })
-  names(explained) <- blearner_names
   
+  ### --------------------------------------------------
+  ### explained risk attributed to baselearners
+  explained <- sapply(seq_along(learner_names), FUN = function(i) {
+    sum( riskdiff[which(learner_selected == i)] ) 
+  })
+  names(explained) <- learner_names
+  
+  
+  ### --------------------------------------------------
+  ### define new varimp-object
   class(explained) <- "varimp"
-  attr(explained, "selprobs") <- sapply(seq_along(blearner_names), function(i) mean(blearner_selected == i))
+  
+  # add selection probabilities to varimp-object
+  attr(explained, "selprobs") <- sapply(seq_along(learner_names), function(i) {
+    mean(learner_selected == i) 
+  })
   
   # add variable names per baselearner
-  variable_names <- variable.names(object)
-  
-  attr(explained, "variables") <- unlist( lapply(variable_names, function(x) {
-    do.call(function(...) paste(..., sep = ","), as.list(x)) 
-  }) )
-  
+  attr(explained, "variable_names") <- unlist( 
+    lapply(variable.names(object) , function(x) {
+      do.call(function(...) paste(..., sep = ","), as.list(x)) 
+    }) 
+  )  
   return(explained)
 }
 
@@ -47,9 +57,17 @@ plot.varimp <- function(x, percent = TRUE, type = "blearner",
   
   args <- as.list(match.call())
   
+  
+  ### --------------------------------------------------
   ### check arguments
   if( !(class(x) == "varimp") ) 
     stop(paste(deparse(substitute(x)), "is not of class varimp."))
+  
+  if( !(is.logical(percent)) )
+     stop("Parameter percent has of type logical.")
+  
+  if( !(type %in% c("blearner","variable")) )
+     stop("Parameter type has to be 'blearner' or 'variable'.")
   
   if( !(is.numeric(nbars) && nbars > 0 && length(nbars) == 1) )
     stop("Parameter nbars has to be a positive integer.")
@@ -60,68 +78,84 @@ plot.varimp <- function(x, percent = TRUE, type = "blearner",
   if( hasArg(xlab) || hasArg(ylab) )
     stop("xlab, ylab already defined by default.")  
   
-  ### set x-axis label
-  if( percent ) xlab = "In-bag Risk Reduction (%)" else 
-    xlab = "In-bag Risk Reduction"
   
+  ### --------------------------------------------------
   ### set range for x-axis
   if( !("xlim" %in% names(args)) ) {
-    if( percent ) xlim = c(0,1)
-    else xlim = c(0,sum(x))
+    if( percent ) xlim <- c(0,1)
+    else xlim <- c(0,sum(x))
   }  
   
-  ### transform to pecent if corresponding argument is true
-  if( percent ) { x <- x / sum(x) }
+  ### set x-axis label and transform values to percentages of necessary
+  if( percent ) {
+    xlab <- "In-bag Risk Reduction (%)"
+    x    <- x / sum(x)
+  } else xlab <- "In-bag Risk Reduction"
   
   
-  # add up values for baselearners that are based on same variable
-  # e.g. bols(var1) and bbs(var1)
+  ### --------------------------------------------------
+  ### create data.frame for all values shown in barchart
+  plot_data <- data.frame(
+    reduction = as.numeric(x), 
+    blearner  = names(x),
+    variable  = attr(x, "variable_names"),
+    selprob   = attr(x, "selprob")
+  )
+  # sort rows by risk reduction (ascending)
+  plot_data_sorted <- plot_data[order(plot_data$reduction),]
+  
+  
+  ### --------------------------------------------------
+  ### if number of baselearners/variables exceeds nbars, aggregate some values
+  plot_data_rows <- nrow(plot_data_sorted)
+  
+  if( plot_data_rows > nbars ) {
+    # add new level OTHER to factor variable blearner/variable
+    plot_data_sorted[, type] <- factor(plot_data_sorted[, type],
+      levels = c(levels(plot_data_sorted[, type]), "other"))  
+    # set baselearner/variable names to OTHER depending on nbars
+    plot_data_sorted[1:(plot_data_rows-nbars+1), type] <- "other"  
+        
+    # add up risk reduction and selprobs for OTHER
+    plot_data_sorted[1:(plot_data_rows-nbars+1), "reduction"] = 
+      sum( head(plot_data_sorted$reduction, plot_data_rows-nbars+1) )
+    plot_data_sorted[1:(plot_data_rows-nbars+1), "selprob"] = 
+      sum( head(plot_data_sorted$selprob, plot_data_rows-nbars+1) )
+    
+    # use only number of observations corresponding to nbars
+    plot_data_sorted = tail(plot_data_sorted, nbars)
+  }
+  
+  
+  ### --------------------------------------------------
+  ### create bar labels
+  # (maybe) truncate labels for bars
+  plot_data_sorted[, type] <- sapply(as.character(plot_data_sorted[, type]), 
+    FUN = function(name) {
+      paste(strtrim(name, maxchar), if( nchar(name) < maxchar ) "" else "..") 
+    }
+  )
+  # add selprobs to bar labels
+  plot_data_sorted[, type] = paste( plot_data_sorted[, type], "\n", 
+    paste0("sel. prob: ~", as.character(round(plot_data_sorted[, "selprob"], 
+    digits = 2))) )
+
+  # use an ordered factor for correct order of bars (descending)
+  plot_data_sorted[, type] = ordered(plot_data_sorted[, type],
+    levels = unique(plot_data_sorted[, type]))
+
+  
+  ### --------------------------------------------------
+  ### create final plot depending on type
   if( type == "variable" ) {
-    
-    variable_order <- order(unlist( lapply(attr(x, "variables"), function(i) {
-      sum(x[which(attr(x, "variables") == i)]) 
-    }) ))
-    
-    # Create data.frame for barchart
-    plot_data <- data.frame(reduction = as.numeric(x), 
-                           blearner = factor(names(x)),
-                           varname = ordered(attr(x, "variables"), 
-      levels = unique(attr(x, "variables")[variable_order])) )
-    
-#     # Sort and accumulate selprobs as character according to variables and in alphabetical order
-#     selprob_sorted <- unlist( lapply(attr(x, "variables"), function(i) {
-#       do.call( function(...) paste( ..., sep = " + " ), as.list( 
-#         attr(x, "selprobs")[attr(x, "variables")==i][order(names(x)[attr(x, "variables")==i])]) ) 
-#     }) )
-#     
-    barchart(varname ~ reduction, groups = blearner, stack = TRUE,
-             horizontal = TRUE, data = plot_data, auto.key = list(), ...)
-  } else
-  
-  {
-  ### sort values
-  xsorted <- sort(x)
-  # if number of baselearners exceeds nbars, aggregate values of other blearners
-  if( length(xsorted) > nbars ) {
-    xsorted <- c( sum( head(xsorted, length(xsorted)-nbars+1) ), 
-      tail(xsorted, nbars-1) ) 
-    names(xsorted)[1] <- "other"
+    barchart(variable ~ reduction, groups = blearner, data = plot_data_sorted,
+      horizontal = TRUE, xlab = xlab, ylab = "Variables", xlim = xlim,
+      scales = list(x = list(tck = c(1,0), at = seq(0,sum(x), length.out = 5))),
+      stack = TRUE, auto.key = list(), ...)
+  } else {
+    barchart(blearner ~ reduction, data = plot_data_sorted,
+      horizontal = TRUE, xlab = xlab, ylab = "Baselearner", xlim = xlim,
+      scales = list(x = list(tck = c(1,0), at = seq(0,sum(x), length.out = 5))),
+      ...)
   }
-  
-  ### create labels for bars
-  names(xsorted) <- sapply(names(xsorted), FUN = function(name) {
-    paste(strtrim(name, maxchar), if( nchar(name) < maxchar ) "" else "..") })
-  
-  # add selection probabilities to bar labels
-  selprob_sorted <- attr(x,"selprob")[order(x, decreasing = FALSE)]
-  # if number of baselearners exceeds nbars, aggregate selprobs of other blearners
-  if( length(selprob_sorted) > nbars ) {
-    selprob_sorted <- c( sum( head(selprob_sorted, length(selprob_sorted)-nbars+1) ), 
-                  tail(selprob_sorted, nbars-1) ) 
-  }
-  names(xsorted) <- paste(names(xsorted), "\n" , paste0("sel. prob: ~", as.character(round(selprob_sorted, digits = 3))) )
-  
-  barchart(x = xsorted, horizontal = TRUE, xlab = xlab, ylab = "Baselearner", 
-    xlim = xlim, ...)
-  }
-}
+}  
