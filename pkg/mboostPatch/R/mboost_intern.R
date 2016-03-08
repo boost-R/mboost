f49396219a06ec39f4bc12649c478e482b47278b
Benjamin Hofner <benjamin.hofner@fau.de>
2016-03-08 10:15:12 +0100

    Updated mboost_intern

--- pkg/mboostPatch/R/mboost_intern.R
+++ pkg/mboostPatch/R/mboost_intern.R
@@ -1,8 +1,8 @@
 mboost_intern <- function(...,
-    fun = c("df2lambda", "hyper_bbs", "bl_lin", "bl_lin_matrix",
+    fun = c("df2lambda", "hyper_bbs", "hyper_ols",
+            "bl_lin", "bl_lin_matrix",
             "Complete.cases", "get_index", "isMATRIX",
-            "hyper_bbs", "cbs", "bsplines", "names.blg",
-            "model.frame.blg")) {
+            "cbs", "bsplines", "model.frame.blg")) {
 
     fun <- match.arg(fun)
     do.call(fun, list(...))
