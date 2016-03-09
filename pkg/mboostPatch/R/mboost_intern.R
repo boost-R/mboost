4d3f57c48a440a657dba6c43c7d2320ea37e05a2
sbrockhaus <sarah.brockhaus@stat.uni-muenchen.de>
2016-03-09 16:57:14 +0100

    add "check_newdata" in mboost_intern()

--- pkg/mboostPatch/R/mboost_intern.R
+++ pkg/mboostPatch/R/mboost_intern.R
@@ -2,7 +2,8 @@ mboost_intern <- function(...,
     fun = c("df2lambda", "hyper_bbs", "hyper_ols",
             "bl_lin", "bl_lin_matrix",
             "Complete.cases", "get_index", "isMATRIX",
-            "cbs", "bsplines", "model.frame.blg")) {
+            "cbs", "bsplines", "model.frame.blg", 
+            "check_newdata")) {
 
     fun <- match.arg(fun)
     do.call(fun, list(...))
