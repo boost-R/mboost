5c503fe9e8d38f52571b44dd2c3efbee1e6305ce
Benjamin Hofner <benjamin.hofner@fau.de>
2016-04-22 17:49:44 +0200

    Removed www site from devel branch

--- www_src/config.R
+++
@@ -1,8 +0,0 @@
-
-pkg <- "mboost"
-dest <- "html"
-publish <- "../www"
-
-download.file("http://user.math.uzh.ch/hothorn/TH.bib", dest = "TH.bib")
-system("cat mboost.bib >> TH.bib")
-bib <- "TH.bib"
