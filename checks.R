af8e49f171d902baca09463c1880e688ed6dc7af
Benjamin Hofner <benjamin.hofner@fau.de>
2017-04-28 14:49:07 +0200

    Updated release procedures

--- checks.R
+++ checks.R
@@ -29,9 +29,11 @@ make_check <- function(srcpkg, dir = "./") {
    check_packages_in_dir(cdir, reverse = list(), Ncpus = 4)
    check_packages_in_dir(ddir, reverse = list(), Ncpus = 4)
 
+   cat("Reverse tests with CRAN package:\n")
    summarize_check_packages_in_dir_results(cdir, all = TRUE)
    summarize_check_packages_in_dir_timings(cdir, all = TRUE)
 
+   cat("Reverse tests with NEW package:\n")
    summarize_check_packages_in_dir_results(ddir, all = TRUE)
    summarize_check_packages_in_dir_timings(ddir, all = TRUE)
 
@@ -39,5 +41,6 @@ make_check <- function(srcpkg, dir = "./") {
    # setwd(odir)
 }
 
-package_dependencies("mboost", available.packages(), reverse = TRUE)
-make_check("mboost")
+# package_dependencies("mboost", available.packages(), reverse = TRUE)
+setwd("../")
+make_check(srcpkg = "mboost_2.8-0.tar.gz")
