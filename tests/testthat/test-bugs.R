92e145f65601f473ae803d8619f6a811021abb09
Benjamin Hofner <benjamin.hofner@pei.de>
2018-06-08 19:10:12 +0200

    (hopefully) fixed new test

--- tests/testthat/test-bugs.R
+++ tests/testthat/test-bugs.R
@@ -3,6 +3,6 @@ require("mboost")
 ### check confidence intervals
 test_that("predict with mstop = 0 works", {
     glm <- glmboost(speed ~ dist, data = cars, control = boost_control(mstop = 0))
-    expect_silent(p <- predict(m, type = "response", newdata = cars))
-    expect_equal(names(p), as.character(1:50))
+    expect_warning(p <- predict(m, type = "response", newdata = cars), regexp  = NA)
+    expect_named(p, as.character(1:50))
 })
